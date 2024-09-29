c49259349cd186aeb870a090be8f2c0b4e7a5b7a
Zayd Alzein <zaydalzein@gmail.com>
2024-09-29 02:44:36 -0500

    debugging kaka setup

--- apps/events-api/main.py
+++ apps/events-api/main.py
@@ -1,30 +1,26 @@
 # app.include_router(token_routes.router, prefix="/api", tags=["tokens"])
 import uvicorn
-from typing import AsyncIterator
 from dotenv import find_dotenv, load_dotenv
 import logging, asyncio, json
 from fastapi import FastAPI
 from colorama import Fore
 from redis import Redis
-from confluent_kafka import Consumer, KafkaError
-
+from confluent_kafka import Consumer, KafkaError, KafkaException
 from contextlib import asynccontextmanager
-import os
+import os, sys, socket
 import datetime
 # from routes import token_routes
 
 logging.basicConfig(level=logging.INFO, format=f'{Fore.MAGENTA}[API]{Fore.RESET} %(asctime)s - %(levelname)s - %(message)s', datefmt='%H:%M:%S')
-load_dotenv(find_dotenv(".env"))
 
+# - - - - - CONFIG - - - - -
+
+load_dotenv(find_dotenv(".env"))
 api_host = os.getenv("API_HOST")
 api_port = os.getenv("API_PORT") 
-
 redis_port = os.getenv("REDIS_PORT")
-
 kafka_remote = os.getenv("KAFKA_BROKER")
 kafka_port = os.getenv("KAFKA_PORT")
-
-redis = Redis(host=str(os.getenv("REDIS_HOST")), port=int(redis_port), db=0) #type: ignore
 
 interested_topics = [
     os.getenv("KAFKA_PRICES_TOPIC"),
@@ -34,67 +30,88 @@ interested_topics = [
     os.getenv("KAFKA_TOKENS_TOPIC"),
 ]
 
-if not api_port:
-    logging.error("API_PORT not found in .env")
-    exit(1)
+event_types = {
+    "prices": os.getenv("KAFKA_PRICES_TOPIC"),
+    "volumes": os.getenv("KAFKA_VOLUMES_TOPIC"),
+    "top_holders": os.getenv("KAFKA_TOP_HOLDERS_TOPIC"),
+    "burns": os.getenv("KAFKA_BURNS_TOPIC"),
+    "tokens": os.getenv("KAFKA_TOKENS_TOPIC"),
+}
 
-if not redis_port:
-    logging.error("REDIS_PORT not found in .env")
-    exit(1)
+#any none true if any intersted topics are none
+any_none = any([not i for i in interested_topics])
+terminate = True if any_none else False
+# terminate if any of the other environment variables are missing
+terminate=True if not all([api_host, api_port, redis_port, kafka_remote, kafka_port, ]) else False
+logging.error("Missing environment variables") if terminate else None
+sys.exit(1) if terminate else None
 
-if not token_stale_channel:
-    logging.error("REDIS_TOKEN_STALE_CHANNEL not found in .env")
-    exit(1)
+# - - - - - END CONFIG - - - - -
 
-async def listen_to_all_topics():
+term_deadline_timestamp = None
+consumer = None
+redis = Redis(host=str(os.getenv("REDIS_HOST")), port=int(redis_port), db=0) #type: ignore
+
+async def pub_to_timescale(data: dict):
+    logging.info(f"Publishing to Timescale: {data}")
+
+
+async def commit_callback(err, partitions):
+    if err:
+        logging.error(f"Commit error: {err}")
+    else:
+        logging.info(f"Committed: {partitions}")
+
+async def kafka_listener():
+    global consumer
+
     try:
-        consumer = Consumer({
-            'bootstrap.servers': f"{kafka_remote}:{kafka_port}",
-            'group.id': 'events-api',
-            'auto.offset.reset': 'earliest'
-        })
-        return consumer
+        logging.info(f"Connecting to Kafka: {kafka_remote}:{kafka_port}")
+        consumer = Consumer(
+            {
+                'bootstrap.servers': kafka_remote,
+                'group.id': 'logger-api',
+                'session.timeout.ms': 6000,
+                'on_commit': commit_callback,
+                'auto.offset.reset': 'earliest'
+            }
+        )
+
+    except KafkaError as e:
+        logging.error(f"KafkaError -> : {e}")
+        exit(1)
     except Exception as e:
         logging.error(f"Error in Kafka consumer: {e}")
         exit(1)
 
-async def pub_to_timescale(data: dict):
-    pass
-
-async def kafka_listener():
-    # kafka consumer task
-    try:
-        consumer = await listen_to_all_topics()
-        consumer.subscribe(interested_topics)
-    except Exception as e:
-        print("FUCK")
-
-    logging.info("Subscribed to topics")
+    if not consumer:
+        logging.error("Consumer not created")
+        exit(1)
+    consumer.subscribe(interested_topics)
+    logging.info(f"Subscribed to topics: {interested_topics}")
 
     while True:
         try:
             message = consumer.poll(timeout=1.0)
 
-            if message is None:
-                continue
+            if message is None: continue
+
             if message.error():
 
-                logging.error(str(message.error()))
-                
-                if "UNKNOWN_TOPIC_OR_PART" in str(message.error()):
-                    logging.fatal("(SETUP) - Need to create topics on Kafka")
-                    exit(1)
+                if message.error().code() == KafkaError._PARTITION_EOF:
+                    logging.error(f"End of partition reached {message.topic()} {message.partition()} {message.offset()}")
+                elif message.error():
+                    logging.error(f"Consumer error: {msg.error()}")
+                    raise KafkaException(message.error())
+                elif "UNKNOWN_TOPIC_OR_PART" in str(message.error()):
+                    logging.info(f"Topic not found: {message.topic()}... making topics: {interested_topics}")
+                    consumer.create_topics(interested_topics)
 
-                logging.error(f"Consumer error: {message.error()}")
-                continue
+            # Process message
+            topic = message.topic(); key = message.key(); value = message.value()
+            logging.info(f"Received message: {topic} {key} {value}")
+            await pub_to_timescale(json.loads(value))
 
-            topic = message.topic()
-            key = message.key()
-            value = message.value()
-            timestamp = datetime.datetime.fromtimestamp(message.timestamp()[1] / 1e3)
-            logging.info(f"Received message: {topic} {key} {value} {timestamp}")
-
-            
         except Exception as e:
             logging.error(f"Error in Kafka listener: {e}")
             exit(1)
@@ -102,7 +119,7 @@ async def kafka_listener():
 
 @asynccontextmanager
 async def start_kafka_listener(application: FastAPI):
-    asyncio.create_task(kafka_listener())  # This will run kafka_listener as a background task
+    await kafka_listener()  # This will run kafka_listener as a background task
     yield
     logging.info("Stopping Kafka listener")
 
@@ -111,9 +128,7 @@ app = FastAPI(lifespan=start_kafka_listener)
 
 if __name__ == "__main__":
     try:
-        logging.info("Starting Kafka listener")
         logging.info("Starting API")
-
         config = uvicorn.Config(app, host=api_host, port=int(api_port), loop="asyncio")
         server = uvicorn.Server(config)
         server.run()
